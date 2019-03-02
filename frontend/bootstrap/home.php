132b4d7c26c03e217de82d9bfabfcdc9bd4ab4bd
juliankanaan <46693750+juliankanaan@users.noreply.github.com>
2019-03-02 12:26:40 -0500

    Create home.php

---
+++ frontend/bootstrap/home.php
@@ -0,0 +1,48 @@
+
+<?php
+/**
+ * Superior to index.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Bootstrap
+ * @since Bootstrap 1.0
+ */
+
+get_header(); ?>
+
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+    <div class="cases">
+
+
+        <div class="card-deck">
+          <div class="card" >
+            <img class="card-img-top" src="https://scontent.fhou1-2.fna.fbcdn.net/v/t1.0-9/22141157_491802567865567_3386180099592378249_n.png?_nc_cat=107&_nc_ht=scontent.fhou1-2.fna&oh=e9d8778a78762e31dac1f1a884262a3e&oe=5C8B667A" alt="Card image cap">
+
+            <div class="card-body">
+              <h5 class="card-title">Case Studies</h5>
+              <p class="card-text">Stories of problem solving from growing <a href="#">Linkcrunch</a> to <mark>3500 MAUs in 40 countries.</mark></p>
+               <a href="case-studies" class="btn btn-primary">Read</a>
+            </div>
+          </div>
+
+
+
+        </div><!-- card group -->
+
+
+  </div>
+
+    
+
+
+
+  </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+
+<?php get_footer(); ?>
