4e2c84dbf2ca8e0a5fe8ae9c165d60802bbf63bc
juliankanaan <46693750+juliankanaan@users.noreply.github.com>
2019-03-02 12:29:44 -0500

    Create footer.php

---
+++ frontend/bootstrap/footer.php
@@ -0,0 +1,35 @@
+
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Bootstrap
+ * @since Bootstrap 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+
+			<!-- btm nav goes here -->
+
+			<div class="container">
+
+				<p class="small" style="text-align:center;">Made by <a href="#">Julian Kanaan</a></p>
+
+				<!-- site info and credits -->
+
+
+			</div><!-- .site-info -->
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
