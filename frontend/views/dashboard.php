64d86c5a30537b24780e29cb31b1fa9088c19f5a
juliankanaan <46693750+juliankanaan@users.noreply.github.com>
2019-03-02 18:36:23 -0500

    Update dashboard.php

--- frontend/views/dashboard.php
+++ frontend/views/dashboard.php
@@ -1,44 +1,47 @@
 
 <?php
-/* Template name: Query Confirm */
+/* Template name: Dashboard */
 
 get_header();
 
-$user_id = get_current_user_id();
-$items = getItems($user_id);
 
 ?>
-<h3>Your entered procedures</h3>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Procedure</th>
-    </tr>
-  </thead>
-  <tbody>
 
+<?php
+$user_id = get_current_user_id();
+$items = getItems($user_id); // get URL param (from previous form entry)
+?>
+
+<h3>Your entered procedures</h3>
+
+<table class="table">
+  <tbody>
     <?php
     if ($items) { // check if results even exist
       foreach ($items as $item) {
-        echo "
-          <tr>
-            <td class='procedure' data-procedure="$item->procedure">$item->procedure></td>
-          </tr>
-             ";
-      } else {
-        echo 'No procedures entered yet!';
+        $confirmed = $item->confirmed;
+        $procedure = $item->procedure_name;
+        if ($confirmed == 0) {
+
+        echo "<tr>
+                <td>$procedure</td>
+                <td>$$item->charge</td>
+                <td><a style='color: white;' class='btn btn-warning' href='refine-action?procedure=$procedure'>Need confirmation!</a></td>
+
+              </tr>";
+        } else {
+          echo "<tr>
+                  <td>$procedure</td>
+                  <td>$$item->charge</td>
+              </tr>";
+
+        }
       }
+    } else {
+      echo 'No procedures entered yet!';
     }
     ?>
-    
   </tbody>
 </table>
-
-
-<div class="next_btns" style="display: none;">
-
-  <button id="new_query">Next</button>
-</div>
-
 
 <?php get_footer(); ?>
