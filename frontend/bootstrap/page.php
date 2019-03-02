a319f8142e7801ccd52be79f38f7e761c816a4df
juliankanaan <46693750+juliankanaan@users.noreply.github.com>
2019-03-02 12:27:57 -0500

    Create page.php

---
+++ frontend/bootstrap/page.php
@@ -0,0 +1,29 @@
+
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Bootstrap
+ * @since Bootstrap 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+    
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
